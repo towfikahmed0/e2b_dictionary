9c7f2c0e1ca8923bb98b05c057247b747e4d4adc
Towfik Ahmed <107678190+towfikahmed0@users.noreply.github.com>
2025-11-17 23:15:36 +0600

    Update service worker cache version to v1.2.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'e2b-dictionary-v1.1.0';
+const CACHE_NAME = 'e2b-dictionary-v1.2.0';
 const urlsToCache = [
   '/',
   './index.html',
@@ -104,4 +104,5 @@ async function updateDictionaryData() {
   }
 
 }
+
 
